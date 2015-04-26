c28c58a5f517f8e5786aaafdd160ea32d41e6b54
Tyrone Neill <tyrone_neill@yahoo.co.uk>
2015-04-26 20:49:45 +0100

    Adding some actual charting elements (not dynamic)

--- app/directive/ticking.chart.js
+++ app/directive/ticking.chart.js
@@ -23,8 +23,11 @@ var com;
                     var _this = this;
                     this.restrict = RESTRICTIONS.ELEMENT;
                     this.scope = { data: '=' };
-                    this.template = "<div class=\"timechart\"></div>";
                     this.link = function ($scope, element, attrs) {
+                        var margin = { top: 30, right: 30, bottom: 30, left: 60 }, width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom;
+                        // set up initial svg object
+                        _this._surface = d3.select(element[0]).append("svg").attr("width", width + margin.left + margin.right).attr("height", height + margin.top + margin.bottom);
+                        // Add listeners.
                         $scope.$watchCollection('data', function (newValue, oldValue) { return _this.invalidateDisplayList(newValue); });
                     };
                 }
@@ -38,12 +41,34 @@ var com;
                     return d;
                 };
                 TickingChart.prototype.invalidateDisplayList = function (value) {
-                    console.log('InvalidateDisplayList called: ' + value.length);
                     var i = 0;
                     while (i < value.length) {
                         console.log(value[i]);
                         i++;
                     }
+                    var margin = { top: 30, right: 30, bottom: 30, left: 60 }, width = 500 - margin.left - margin.right, height = 300 - margin.top - margin.bottom;
+                    var data = [
+                        [12345, 42345, 3234, 22345, 72345, 62345, 32345, 92345, 52345, 22345],
+                        [1234, 4234, 3234, 2234, 7234, 6234, 3234, 9234, 5234, 2234]
+                    ];
+                    var x = d3.scale.linear().domain([0, data[0].length]).range([0, width]), y = d3.scale.linear().domain([0, d3.max(data[0])]).range([height, 0]), xAxis = d3.svg.axis().scale(x).ticks(10), yAxis = d3.svg.axis().scale(y).ticks(10).orient("left");
+                    var line = d3.svg.line().x(function (d, i) {
+                        return x(i);
+                    }).y(function (d) {
+                        return y(d);
+                    });
+                    var area = d3.svg.area().x(line.x()).y1(line.y()).y0(y(0));
+                    var lines = this._surface.selectAll("g").data(data);
+                    var aLineContainer = lines.enter().append("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+                    aLineContainer.append("path").attr("class", "area").attr("d", area);
+                    aLineContainer.append("path").attr("class", "line").attr("d", line);
+                    aLineContainer.selectAll(".dot").data(function (d, i) {
+                        return d;
+                    }).enter().append("circle").attr("class", "dot").attr("cx", line.x()).attr("cy", line.y()).attr("r", 3.0);
+                    // Add the x-axis.
+                    this._surface.append("g").attr("class", "x axis").attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")").call(xAxis);
+                    // Add the y-axis.
+                    this._surface.append("g").attr("class", "y axis").attr("transform", "translate(" + margin.left + "," + margin.top + ")").call(yAxis);
                 };
                 return TickingChart;
             })();
--- app/directive/ticking.chart.ts
+++ app/directive/ticking.chart.ts
@@ -5,6 +5,10 @@
 module com.uk.grapevine {
     'use strict';
 
+    import LinearScale = D3.Scale.LinearScale;
+    import Axis = D3.Svg.Axis;
+    import Line = D3.Svg.Line;
+
     export class RESTRICTIONS {
         static ELEMENT:string = "E";
         static ATTRIBUTE:string = "A";
@@ -30,13 +34,25 @@ module com.uk.grapevine {
 
         public restrict:string = RESTRICTIONS.ELEMENT;
         public scope = {data: '='};
-        public template:string = "<div class=\"timechart\"></div>";
         public link:($scope:DataScope<Article>, element:ng.IAugmentedJQuery, attrs:ng.IAttributes) => void;
+
+        private _surface:D3.Selection;
 
         constructor() {
 
             this.link = ($scope:DataScope<Article>, element:ng.IAugmentedJQuery, attrs:ng.IAttributes) => {
 
+                var margin = {top: 30, right: 30, bottom: 30, left: 60},
+                    width = 500 - margin.left - margin.right,
+                    height = 300 - margin.top - margin.bottom;
+
+                // set up initial svg object
+                this._surface = d3.select(element[0])
+                    .append("svg")
+                    .attr("width", width + margin.left + margin.right)
+                    .attr("height", height + margin.top + margin.bottom)
+
+                // Add listeners.
                 $scope.$watchCollection(
                     'data',
                     (newValue:Article[], oldValue:Article[]) => this.invalidateDisplayList(newValue)
@@ -45,13 +61,78 @@ module com.uk.grapevine {
         }
 
         private invalidateDisplayList(value:Array<any>):void {
-            console.log('InvalidateDisplayList called: ' + value.length)
 
             var i:number = 0;
             while (i < value.length) {
                 console.log(value[i])
                 i++;
             } // End of loop.
+
+            var margin = {top: 30, right: 30, bottom: 30, left: 60},
+                width = 500 - margin.left - margin.right,
+                height = 300 - margin.top - margin.bottom;
+
+            var data = [
+                [12345, 42345, 3234, 22345, 72345, 62345, 32345, 92345, 52345, 22345],
+                [1234, 4234, 3234, 2234, 7234, 6234, 3234, 9234, 5234, 2234]
+            ];
+
+            var x:LinearScale = d3.scale.linear().domain([0, data[0].length]).range([0, width]),
+                y:LinearScale = d3.scale.linear().domain([0, d3.max(data[0])]).range([height, 0]),
+                xAxis:Axis = d3.svg.axis().scale(x).ticks(10),
+                yAxis:Axis = d3.svg.axis().scale(y).ticks(10).orient("left");
+
+            var line = d3.svg.line()
+                .x(function (d, i) {
+                    return x(i);
+                })
+                .y(function (d) {
+                    return y(d);
+                });
+
+            var area = d3.svg.area()
+                .x(line.x())
+                .y1(line.y())
+                .y0(y(0));
+
+            var lines = this._surface.selectAll("g")
+                .data(data);
+
+            var aLineContainer = lines.enter().append("g")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+            aLineContainer
+                .append("path")
+                .attr("class", "area")
+                .attr("d", area);
+
+            aLineContainer
+                .append("path")
+                .attr("class", "line")
+                .attr("d", line);
+
+            aLineContainer.selectAll(".dot")
+                .data(function (d, i) {
+                    return d;
+                })
+                .enter()
+                .append("circle")
+                .attr("class", "dot")
+                .attr("cx", line.x())
+                .attr("cy", line.y())
+                .attr("r", 3.0);
+
+            // Add the x-axis.
+            this._surface.append("g")
+                .attr("class", "x axis")
+                .attr("transform", "translate(" + margin.left + "," + (height + margin.top) + ")")
+                .call(xAxis);
+
+            // Add the y-axis.
+            this._surface.append("g")
+                .attr("class", "y axis")
+                .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+                .call(yAxis);
         }
     }
 }
